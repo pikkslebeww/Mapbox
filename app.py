53343622e580b97f1c47faeda121fe69c6fd53e7
pikkslebeww <noviansyahh2006@gmail.com>
2023-11-20 11:36:53 +0700

    week15

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 from flask import (
     Flask,
     render_template,
@@ -9,8 +12,16 @@ from flask import (
 import certifi
 from pymongo import MongoClient
 
-app = Flask(__name__)
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
+MONGODB_URI= os.environ.get("MONGODB_URI")
+DB_NAME = os.environ.get("DB_NAME")
+
+client = MongoClient (MONGODB_URI)
+db = client [DB_NAME]
+ 
+app = Flask(__name__)
 password = 'Jakmania1928'
 cxn_str = f'mongodb+srv://noviansyahh2006:{password}@cluster0.z6ucths.mongodb.net/'
 client = MongoClient(cxn_str)
